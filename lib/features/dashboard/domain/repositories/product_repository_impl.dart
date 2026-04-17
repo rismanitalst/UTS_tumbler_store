6117c8434bb8ee98f981ebd546c8240994eb576d
rismanitalst <rismanitalst@gmail.com>
2026-04-17 11:56:09 +0700

    feat: Domain/Model Product
    menambahkan product repository impl.

---
+++ lib/features/dashboard/domain/repositories/product_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'package:tumbler_store/core/constants/api_constants.dart';
+import 'package:tumbler_store/core/services/dio_client.dart';
+import 'package:tumbler_store/features/dashboard/data/models/product_models.dart';
+import 'package:tumbler_store/features/dashboard/domain/repositories/product_repository.dart';
+
+class ProductRepositoryImpl implements ProductRepository {
+  @override
+  Future<List<ProductModel>> getProducts({
+    int page = 1,
+    int limit = 10,
+    String? category,
+  }) async {
+    final response = await DioClient.instance.get(
+      ApiConstants.products,
+      queryParameters: {'page': page, 'limit': limit, 'category': category},
+    );
+
+    final List<dynamic> data = response.data['data'];
+    return data.map((e) => ProductModel.fromJson(e)).toList();
+  }
+
+  @override
+  Future<ProductModel> getProductById(int id) async {
+    final response = await DioClient.instance.get(
+      '${ApiConstants.products}/$id',
+    );
+    return ProductModel.fromJson(response.data['data']);
+  }
+}
